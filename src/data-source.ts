ca1f660e798112afa9b1fc9331e0fd796aa700b5
Taylor Hayduk <taylorhayduk@gmail.com>
2024-02-13 09:28:54 -0500

    use in-memory sql for ease of use

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,29 +4,40 @@ import { Menu } from "./entity/Menu";
 import { MenuItem } from "./entity/MenuItem";
 import { Restaurant } from "./entity/Restaurant";
 
-// Determine if we are in a test environment
-const isTestEnvironment = process.env.NODE_ENV === "test";
-
 export const AppDataSource = new DataSource({
-  // Use SQLite for testing and PostgreSQL for other environments
-  type: isTestEnvironment ? "sqlite" : "postgres",
-  // Database connection configuration varies based on the type
-  database: isTestEnvironment ? ":memory:" : "popmenu",
+  // Use SQLite for both testing and non-testing environments
+  type: "sqlite",
+  database: ":memory:",
   entities: [Menu, MenuItem, Restaurant],
-
-  // When using SQLite for tests
-  ...(isTestEnvironment && {
-    synchronize: true, // Automatically create the database schema on startup
-    logging: false, // Reduce log output for tests
-  }),
-
-  // When using PostgreSQL (non-testing environments)
-  ...(!isTestEnvironment && {
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "mypass",
-    synchronize: true, // Be careful with this in production!
-    logging: false,
-  }),
+  synchronize: true, // Automatically create the database schema on startup
+  logging: false, // Reduce log output
 });
+
+// ACTUAL Setup if deploying to a real database
+
+// Determine if we are in a test environment
+// const isTestEnvironment = process.env.NODE_ENV === "test";
+
+// export const AppDataSource = new DataSource({
+//   // Use SQLite for testing and PostgreSQL for other environments
+//   type: isTestEnvironment ? "sqlite" : "postgres",
+//   // Database connection configuration varies based on the type
+//   database: isTestEnvironment ? ":memory:" : "popmenu",
+//   entities: [Menu, MenuItem, Restaurant],
+
+//   // When using SQLite for tests
+//   ...(isTestEnvironment && {
+//     synchronize: true, // Automatically create the database schema on startup
+//     logging: false, // Reduce log output for tests
+//   }),
+
+//   // When using PostgreSQL (non-testing environments)
+//   ...(!isTestEnvironment && {
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "mypass",
+//     synchronize: false, // We would use /src/migrations for schema changes
+//     logging: false,
+//   }),
+// });
